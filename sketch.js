74b41d223ecf8d275f82ff5d83001d19c6cf82ba
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-11-02 14:37:58 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ function setup() {
   ball = new Ball(width / 2 + 80, height / 2 - 80, 80, 80);
   blower = new Blower(width / 2 - 50, height / 2 + 50, 150, 20);
   blowerMouth = new BlowerMouth(width / 2 + 70, height / 2 + 20, 100, 90);
-  button = createButton("Click to Blow");
+  button = createButton("Clique para Assoprar");
   button.position(width / 2, height - 100);
   button.class("blowButton");
 
