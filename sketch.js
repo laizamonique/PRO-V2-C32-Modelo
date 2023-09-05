176f8daf113f3e8a97b56a741a460f564d65ca88
laizamonique <133267385+laizamonique@users.noreply.github.com>
2023-09-05 16:36:47 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function setup() {
   button.position(width / 2, height - 100);
   button.class("blowButton");
 
-  //button.mousePressed(blow);
+  button.mousePressed(blow);
 
   //buttonPressed(blow);
   
@@ -50,7 +50,7 @@ function blow() {
 
   //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:-0.05});
 
-  //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:0.05});
+  Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:0.05});
   
   //Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0.05, y:0.05});
   
